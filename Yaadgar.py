5d66de7dfb4ccce13b11d3ebacaf20c9b9cd7992
adnankoko <136540646+adnankoko@users.noreply.github.com>
2023-06-14 07:22:42 +0500

    Create Yaadgar.py2

---
+++ Yaadgar.py
@@ -0,0 +1,21 @@
+pkg upgrade 
+
+pkg update 
+
+pkg install git 
+
+pkg install python 
+
+pip install requests
+
+pip install mechanize
+
+pip install bs4
+
+pip install future
+
+git clone https://github.com/adnankoko/Yaadgar.git
+
+Cd Yaadgar1
+
+Python2 Yaadgar1.py
